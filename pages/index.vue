85c282f1f80351b4ffac92bad788153183714de5
Ravindra Sisodia <ravindraatalinasoftware@gmail.com>
2019-12-03 14:07:42 +0530

    Minor Changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container">
-    <div class="logo">
-      <img src="/images/logo.png" alt="">
+    <div class="div">
+      <img src="/images/back.png">
+    </div>
+    <div class="logo_container">
+      <img class="logo" src="/images/logo.png" alt="">
     </div>
 
-    <h5>Sign in</h5>
-    <input class="input_control" type="text" id="name" placeholder="John Doe">
-    <input class="input_control" type="password" id="password" placeholder="********">
-    <label>
+    <h4>Sign in</h4>
+    <input class="input_control" type="text" id="name" placeholder="Username">
+    <input class="input_control" type="password" id="password" placeholder="Password">
+    <label id="rememberMeLabel">
       <input type="checkbox" id="rememberMe"> Stay logged in 
     </label>
     <button>Sign in</button>
@@ -21,29 +24,43 @@ export default {}
 </script>
 
 <style>
-img {
-  width: 100%;
+@import url('https://fonts.googleapis.com/css?family=Montserrat:200,300,400&display=swap');
+* {
+  font-family: 'Montserrat', sans-serif;
 }
 
 .logo {
-  padding: 10px 20px 80px;
+  width: 100%;
+}
+
+.logo_container {
+  margin-top: 50px;;
+  padding: 10px 0px 80px;
 }
 
 .text-center {
   text-align: center;
 }
 
-h5 {
+h4 {
   font-weight: 300;
   margin-bottom: 20px;
 }
 
 .input_control {
+  outline: none;
   border-radius: 15px;
-  border: solid #03be33 1px; 
-  padding: 10px;
+  font-size: 15px;
+  font-weight: 100;
+  border: solid #EEE 1px; 
+  padding: 15px;
   margin-bottom: 20px;
   width: 100%; 
+}
+
+.input_control:focus {
+  outline: none;
+  border: solid #03be33 1px; 
 }
 
 button {
@@ -53,7 +70,8 @@ button {
   border-radius: 15px;
   width: 100%;
   font-size: 15px;
-  padding: 10px;
+  font-weight: 700;
+  padding: 15px;
   border: none;
   margin-top: 15px;
 }
@@ -63,10 +81,15 @@ a, a:hover, a:visited {
 }
 
 .forget_password_container {
-  padding-top: 10px;
+  padding: 20px;
 }
 
 #rememberMe {
   border-radius: 25%;
+  margin-bottom: 20px;
+}
+
+#rememberMeLabel {
+  font-weight: 100;
 }
 </style>
